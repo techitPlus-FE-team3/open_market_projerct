4ae57a35e597a4cbd278d3d9dd3ec777918484b3
s-ja <dkstmdwl0615@naver.com>
2023-11-22 14:26:58 +0900

    :label: [Type] 헬멧 타입 문제 해결

--- open-market/src/main.tsx
+++ open-market/src/main.tsx
@@ -2,21 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { HelmetProvider } from "react-helmet-async";
+import { HelmetProvider, HelmetServerState } from "react-helmet-async";
 import { Toaster } from "react-hot-toast";
 import { renderToString } from "react-dom/server";
 
-const helmetContext = {};
+const helmetContext: { helmet: HelmetServerState } = {
+    helmet: {} as HelmetServerState,
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<HelmetProvider context={helmetContext}>
-		<React.StrictMode>
-			<Toaster />
-			<App />
-		</React.StrictMode>
-	</HelmetProvider>,
+    <HelmetProvider context={helmetContext}>
+        <React.StrictMode>
+            <Toaster />
+            <App />
+        </React.StrictMode>
+    </HelmetProvider>,
 );
 
-const html = renderToString();
+const html = renderToString(<App />);
 
 const { helmet } = helmetContext;
