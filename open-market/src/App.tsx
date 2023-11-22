651c98a6520e5df8ff822d1178ebd3de3f788fc4
Pearl Kim <jinju000202@gmail.com>
2023-11-22 15:51:45 +0900

    :bug: [Fix] HelmetProvider에서 add 속성을 찾지 못하는 에러 수정

--- open-market/src/App.tsx
+++ open-market/src/App.tsx
@@ -1,15 +1,20 @@
-import { Helmet } from "react-helmet-async";
+import { Helmet, HelmetProvider, HelmetServerState } from "react-helmet-async";
 import { RecoilRoot } from "recoil";
-
 function App() {
+	const helmetContext: { helmet: HelmetServerState } = {
+		helmet: {} as HelmetServerState,
+	};
+
 	return (
-		<RecoilRoot>
-			<div className="App">
-				<Helmet>
-					<title>모두의 오디오 : 모디</title>
-				</Helmet>
-			</div>
-		</RecoilRoot>
+		<HelmetProvider context={helmetContext}>
+			<RecoilRoot>
+				<div className="App">
+					<Helmet>
+						<title>모두의 오디오 : 모디</title>
+					</Helmet>
+				</div>
+			</RecoilRoot>
+		</HelmetProvider>
 	);
 }
 
--- open-market/src/main.tsx
+++ open-market/src/main.tsx
@@ -1,24 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import App from "./App";
 import "./index.css";
-import { HelmetProvider, HelmetServerState } from "react-helmet-async";
-import { Toaster } from "react-hot-toast";
-import { renderToString } from "react-dom/server";
-
-const helmetContext: { helmet: HelmetServerState } = {
-    helmet: {} as HelmetServerState,
-};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <HelmetProvider context={helmetContext}>
-        <React.StrictMode>
-            <Toaster />
-            <App />
-        </React.StrictMode>
-    </HelmetProvider>,
+	<React.StrictMode>
+		<Toaster />
+		<App />
+	</React.StrictMode>,
 );
-
-const html = renderToString(<App />);
-
-const { helmet } = helmetContext;
