178896e6cb3cdc5a38362104da362608860a8bf8
hungerbk <wjantlr@gmail.com>
2023-11-23 16:27:42 +0900

    :sparkles:[Feature] 회원정보 수정 페이지 라우트 추가

--- open-market/src/routes.tsx
+++ open-market/src/routes.tsx
@@ -1,13 +1,14 @@
 import RootLayout from "@/layout/RootLayout";
 import Index from "@/pages/Index";
-import SignIn from "@/pages/SignIn";
 import ProductRegistration from "@/pages/ProductRegistration";
+import SignIn from "@/pages/SignIn";
 import SignUp from "@/pages/SignUp";
 import {
 	Route,
 	createBrowserRouter,
 	createRoutesFromElements,
 } from "react-router-dom";
+import UserUpdate from "./pages/UserUpdate";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -16,6 +17,7 @@ const router = createBrowserRouter(
 			<Route path="signin" element={<SignIn />} />
 			<Route path="registration" element={<ProductRegistration />} />
 			<Route path="signup" element={<SignUp />} />
+			<Route path="userUpdate" element={<UserUpdate />} />
 		</Route>,
 	),
 );
