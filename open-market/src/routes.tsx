d4e030243d01f36a9cf026e3705a0d704929995f
Pearl Kim <jinju000202@gmail.com>
2023-11-23 14:46:22 +0900

    :sparkles: [Feature] 라우터에 RootLayout 생성 및 연결

--- open-market/src/routes.tsx
+++ open-market/src/routes.tsx
@@ -4,11 +4,14 @@ import {
 	createBrowserRouter,
 	createRoutesFromElements,
 } from "react-router-dom";
+import ProductRegistration from "@/pages/ProductRegistration";
+import RootLayout from "@/layout/RootLayout";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
-		<Route path="/" element={<Index />}>
+		<Route path="/" element={<RootLayout />}>
 			<Route index element={<Index />} />
+			<Route path="registration" element={<ProductRegistration />} />
 		</Route>,
 	),
 );
