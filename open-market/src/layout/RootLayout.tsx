2c013d2d344e5bb7f0df249976ba32b6c123ff9c
s-ja <dkstmdwl0615@naver.com>
2023-11-23 17:09:30 +0900

    🐛 [Fix] 상대경로, 절대경로로 수정

--- open-market/src/layout/RootLayout.tsx
+++ open-market/src/layout/RootLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/layout/Header";
-import Footer from "@layout/Footer";
+import Footer from "@/layout/Footer";
 
 function RootLayout() {
 	return (
