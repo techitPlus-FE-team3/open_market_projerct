c6d2493f7f1f2b95721eb21dbfc61158ea938e19
ASJ <dkstmdwl0615@naver.com>
2023-11-23 17:32:57 +0900

    Design/header footer (#28)
    * 💄 [Design] 헤더/푸터 마크업 작업중...
    
    * 💄 [Design] 헤더 요소 나열, 푸터 임시 작업
    
    * 🐛 [Fix] 상대경로, 절대경로로 수정
    
    * 🐛 [Fix] 상대경로, 절대경로로 수정
    
    * 💄 [Design] 헤더 임시 스타일링 추가

--- open-market/src/layout/Header.tsx
+++ open-market/src/layout/Header.tsx
@@ -1,24 +1,115 @@
 import React from "react";
+import styled from "@emotion/styled";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
+
+const StyledHeader = styled.header`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 1rem 2rem;
+	background-color: #fff;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const Logo = styled.h1`
+	font-size: 1.5rem;
+	a {
+		text-decoration: none;
+		color: inherit;
+		display: flex;
+		align-items: center;
+
+		img {
+			height: 40px;
+		}
+	}
+`;
+
+const SearchForm = styled.form`
+	display: flex;
+	align-items: center;
+	flex-grow: 1;
+	input {
+		flex-grow: 1;
+		padding: 0.5rem 1rem;
+		border: 1px solid #ddd;
+		border-radius: 20px;
+		margin-right: 0.5rem;
+	}
+
+	button {
+		padding: 0.5rem 1rem;
+		background-color: #007bff;
+		border: none;
+		border-radius: 20px;
+		color: white;
+		cursor: pointer;
+
+		&:hover {
+			background-color: #0056b3;
+		}
+	}
+`;
+
+const ActionGroup = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+	gap: 1rem;
+`;
+
+const UploadButton = styled.button`
+	display: flex;
+	align-items: center;
+	padding: 0.5rem 1rem;
+	background-color: #28a745;
+	border: none;
+	border-radius: 20px;
+	color: white;
+	cursor: pointer;
+
+	svg {
+		margin-right: 0.5rem;
+	}
+
+	&:hover {
+		background-color: #1e7e34;
+	}
+`;
+
+const LoginButton = styled.button`
+	padding: 0.5rem 1rem;
+	background-color: #6c757d;
+	border: none;
+	border-radius: 20px;
+	color: white;
+	cursor: pointer;
+
+	&:hover {
+		background-color: #5a6268;
+	}
+`;
 
 const Header = () => {
 	return (
-		<header>
-			<h1>
-				<a href="">
-					<img src="" alt="모디 로고" />
+		<StyledHeader>
+			<Logo>
+				<a href="/">
+					<img src="/logo.png" alt="모디 로고" />
 				</a>
-			</h1>
-			<form action="">
+			</Logo>
+			<SearchForm action="">
 				<input type="text" placeholder="검색어를 입력하세요" />
 				<button type="submit">검색</button>
-			</form>
-			<button>
-				<FileUploadIcon />
-				업로드
-			</button>
-			<button>로그인 / 회원가입</button>
-		</header>
+			</SearchForm>
+			<ActionGroup>
+				<UploadButton>
+					<FileUploadIcon fontSize="small" />
+					업로드
+				</UploadButton>
+				<LoginButton>로그인 / 회원가입</LoginButton>
+			</ActionGroup>
+		</StyledHeader>
 	);
 };
 
