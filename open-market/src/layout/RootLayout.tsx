d66f5232de09178bb129259ed520b7367170f4aa
s-ja <dkstmdwl0615@naver.com>
2023-11-23 16:51:22 +0900

    💄 [Design] 헤더/푸터 마크업 작업중...

--- open-market/src/layout/RootLayout.tsx
+++ open-market/src/layout/RootLayout.tsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function RootLayout() {
 	return (
-		<div>
-			<Outlet />
-		</div>
+		<>
+			<Header />
+			<main>
+				<Outlet />
+			</main>
+			<Footer />
+		</>
 	);
 }
 
