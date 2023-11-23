2a6f483ac3141e3693b60a1b4934a357c233033e
hungerbk <wjantlr@gmail.com>
2023-11-23 15:30:31 +0900

    :truck:[Rename] Login을 SignIn으로 수정

--- open-market/src/routes.tsx
+++ open-market/src/routes.tsx
@@ -1,9 +1,8 @@
 import RootLayout from "@/layout/RootLayout";
 import Index from "@/pages/Index";
-import Login from "@/pages/Login";
+import SignIn from "@/pages/SignIn";
 import ProductRegistration from "@/pages/ProductRegistration";
 import SignUp from "@/pages/SignUp";
-import Login from "@/pages/Login";
 import {
 	Route,
 	createBrowserRouter,
@@ -14,10 +13,9 @@ const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<RootLayout />}>
 			<Route index element={<Index />} />
-			<Route path="login" element={<Login />} />
+			<Route path="signin" element={<SignIn />} />
 			<Route path="registration" element={<ProductRegistration />} />
 			<Route path="signup" element={<SignUp />} />
-			<Route path="login" element={<Login />} />
 		</Route>,
 	),
 );
