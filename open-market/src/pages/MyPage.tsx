7a76fcb2843d6fed26561d0c0607dab9e7e64eed
s-ja <dkstmdwl0615@naver.com>
2023-11-24 15:38:36 +0900

    🐛 [Fix] h1 태그 중복으로 인한 태그 변경, a 태그에서 Link 태그로 변경

--- open-market/src/pages/MyPage.tsx
+++ open-market/src/pages/MyPage.tsx
@@ -3,168 +3,168 @@ import React from "react";
 const MyPage = () => {
 	return (
 		<section>
-			<h1>마이페이지</h1>
+			<h2>마이페이지</h2>
 			<article>
-				<h2>내 정보</h2>
+				<h3>내 정보</h3>
 				<img src="" alt="회원 썸네일" />
 				<div>
-                    <div>
-                        <h3>회원정보</h3>
-                        <div>
-                            <h4>이메일</h4>
-                            <p>dkstmdwl0615@naver.com</p>
-                        </div>
-                        <div>
-                            <h4>비밀번호</h4>
-                            <p>*******</p>
-                        </div>
-                        <div>
-                            <h4>이름</h4>
-                            <p>안승지</p>
-                        </div>
-                        <div>
-                            <h4>전화 번호</h4>
-                            <p>010-8642-****</p>
-                        </div>
-                    </div>
-                    <div>
-                        <h3>내가 쓴 댓글</h3>
-                        <div>
-                            <h4>게시글 제목</h4>
-                            <p>내용</p>
-                        </div>
-                        <div>
-                            <h4>게시글 제목</h4>
-                            <p>내용</p>
-                        </div>
-                        <div>
-                            <h4>게시글 제목</h4>
-                            <p>내용</p>
-                        </div>
-                        <button>회원정보 수정</button>
-                    </div>
-                </div>
+					<div>
+						<h4>회원정보</h4>
+						<div>
+							<h5>이메일</h5>
+							<p>dkstmdwl0615@naver.com</p>
+						</div>
+						<div>
+							<h5>비밀번호</h5>
+							<p>*******</p>
+						</div>
+						<div>
+							<h5>이름</h5>
+							<p>안승지</p>
+						</div>
+						<div>
+							<h5>휴대폰 번호</h5>
+							<p>010-8642-****</p>
+						</div>
+					</div>
+					<div>
+						<h4>내가 쓴 댓글</h4>
+						<div>
+							<h5>게시글 제목</h5>
+							<p>내용</p>
+						</div>
+						<div>
+							<h5>게시글 제목</h5>
+							<p>내용</p>
+						</div>
+						<div>
+							<h5>게시글 제목</h5>
+							<p>내용</p>
+						</div>
+						<button>회원정보 수정</button>
+					</div>
+				</div>
 			</article>
 			<article>
-				<h2>북마크</h2>
+				<h3>북마크</h3>
 				<ul>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 				</ul>
 				<button>전체보기</button>
 			</article>
 			<article>
-				<h2>히스토리</h2>
+				<h3>히스토리</h3>
 				<ul>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 				</ul>
 				<button>전체보기</button>
 			</article>
 			<article>
-				<h2>구매내역</h2>
+				<h3>구매내역</h3>
 				<ul>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 				</ul>
 				<button>전체보기</button>
 			</article>
 			<article>
-				<h2>판매내역</h2>
+				<h3>판매내역</h3>
 				<ul>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 					<li>
-						<a href="/">
+						<Link to="/">
 							<img src="" alt="앨범아트" />
-						</a>
+						</Link>
 					</li>
 				</ul>
 				<button>전체보기</button>
