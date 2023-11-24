0dad1a93d0d55f47635be264f394d325d3fa2e9b
Bokyeong Kang <43366461+hungerbk@users.noreply.github.com>
2023-11-24 15:55:09 +0900

    💄[Design] 판매상품 관리 페이지 마크업 #24 (#36)
    * :lipstick:[Design] 판매상품 관리 페이지 마크업 #24
    
    * :sparkles:[Feature] 판매관리페이지 라우트 추가

--- open-market/src/routes.tsx
+++ open-market/src/routes.tsx
@@ -1,19 +1,20 @@
 import RootLayout from "@/layout/RootLayout";
 import Detail from "@/pages/Detail";
 import Index from "@/pages/Index";
+import MyPage from "@/pages/MyPage";
 import ProductEdit from "@/pages/ProductEdit";
 import ProductRegistration from "@/pages/ProductRegistration";
 import Purchase from "@/pages/Purchase";
 import SignIn from "@/pages/SignIn";
 import SignUp from "@/pages/SignUp";
 import UserUpdate from "@/pages/UserUpdate";
-import MyPage from "@/pages/MyPage";
 import {
 	Route,
 	createBrowserRouter,
 	createRoutesFromElements,
 } from "react-router-dom";
 import ProductManage from "./pages/ProductManage";
+import UserProducts from "./pages/UserProducts";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -28,6 +29,7 @@ const router = createBrowserRouter(
 			<Route path="update/userId" element={<UserUpdate />} />
 			<Route path="productpurchase/:productId" element={<Purchase />} />
 			<Route path="productmanage/:productId" element={<ProductManage />} />
+			<Route path="user/userId/products" element={<UserProducts />} />
 		</Route>,
 	),
 );
