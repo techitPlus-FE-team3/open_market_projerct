e49b5c38b101b258ccb68d032d4feb63614748e5
s-ja <dkstmdwl0615@naver.com>
2023-11-24 15:16:07 +0900

    💄 [Design] 마이페이지 마크업

---
+++ open-market/src/pages/MyPage.tsx
@@ -0,0 +1,176 @@
+import React from "react";
+
+const MyPage = () => {
+	return (
+		<section>
+			<h1>마이페이지</h1>
+			<article>
+				<h2>내 정보</h2>
+				<img src="" alt="회원 썸네일" />
+				<div>
+                    <div>
+                        <h3>회원정보</h3>
+                        <div>
+                            <h4>이메일</h4>
+                            <p>dkstmdwl0615@naver.com</p>
+                        </div>
+                        <div>
+                            <h4>비밀번호</h4>
+                            <p>*******</p>
+                        </div>
+                        <div>
+                            <h4>이름</h4>
+                            <p>안승지</p>
+                        </div>
+                        <div>
+                            <h4>전화 번호</h4>
+                            <p>010-8642-****</p>
+                        </div>
+                    </div>
+                    <div>
+                        <h3>내가 쓴 댓글</h3>
+                        <div>
+                            <h4>게시글 제목</h4>
+                            <p>내용</p>
+                        </div>
+                        <div>
+                            <h4>게시글 제목</h4>
+                            <p>내용</p>
+                        </div>
+                        <div>
+                            <h4>게시글 제목</h4>
+                            <p>내용</p>
+                        </div>
+                        <button>회원정보 수정</button>
+                    </div>
+                </div>
+			</article>
+			<article>
+				<h2>북마크</h2>
+				<ul>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+				</ul>
+				<button>전체보기</button>
+			</article>
+			<article>
+				<h2>히스토리</h2>
+				<ul>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+				</ul>
+				<button>전체보기</button>
+			</article>
+			<article>
+				<h2>구매내역</h2>
+				<ul>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+				</ul>
+				<button>전체보기</button>
+			</article>
+			<article>
+				<h2>판매내역</h2>
+				<ul>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src="" alt="앨범아트" />
+						</a>
+					</li>
+				</ul>
+				<button>전체보기</button>
+			</article>
+		</section>
+	);
+};
+
+export default MyPage;
