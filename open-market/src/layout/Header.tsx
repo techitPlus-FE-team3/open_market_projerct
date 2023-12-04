74f345de8b96506cf30281299f95a63ce383e037
s-ja <dkstmdwl0615@naver.com>
2023-12-04 14:44:07 +0900

    🐛 [Fix] 헤더 로고 이미지 로딩 전 헤더 렌더링 완료시 엑박 문제 #144

--- open-market/src/layout/Header.tsx
+++ open-market/src/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import {
 	AppBar,
@@ -9,6 +9,7 @@ import {
 	TextField,
 	InputAdornment,
 	Badge,
+	Button,
 } from "@mui/material";
 import {
 	AccountCircle,
@@ -23,15 +24,6 @@ import { useRecoilState } from "recoil";
 import { loggedInState } from "../states/authState";
 import toast from "react-hot-toast";
 
-const StyledHeader = styled.header`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	padding: 1rem 2rem;
-	background-color: #fff;
-	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`;
-
 const Logo = styled.h1`
 	a {
 		text-decoration: none;
@@ -45,102 +37,27 @@ const Logo = styled.h1`
 	}
 `;
 
-const SearchForm = styled.form`
-	display: flex;
-	align-items: center;
-	flex-grow: 1;
-	input {
-		flex-grow: 1;
-		padding: 0.5rem 1rem;
-		border: 1px solid #ddd;
-		border-radius: 20px;
-		margin-right: 0.5rem;
-	}
-
-	button {
-		padding: 0.5rem 1rem;
-		background-color: #007bff;
-		border: none;
-		border-radius: 20px;
-		color: white;
-		cursor: pointer;
-
-		&:hover {
-			background-color: #0056b3;
-		}
-	}
-`;
-
-const ActionGroup = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: flex-end;
-	gap: 1rem;
-`;
-
-const UploadButton = styled(Link)`
-	display: flex;
-	align-items: center;
-	padding: 0.5rem 1rem;
-	background-color: #28a745;
-	border: none;
-	border-radius: 20px;
-	color: white;
-	cursor: pointer;
-
-	svg {
-		margin-right: 0.5rem;
-	}
-
-	&:hover {
-		background-color: #1e7e34;
-	}
-`;
-
-const LoginButton = styled(Link)`
-	padding: 0.5rem 1rem;
-	background-color: #6c757d;
-	border: none;
-	border-radius: 20px;
-	color: white;
-	cursor: pointer;
-
-	&:hover {
-		background-color: #5a6268;
-	}
-`;
-
-const MyPageButton = styled(Link)`
-	padding: 0.5rem 1rem;
-	background-color: #6c757d;
-	border: none;
-	border-radius: 20px;
-	color: white;
-	cursor: pointer;
-
-	&:hover {
-		background-color: #5a6268;
-	}
-`;
-
-const LogoutButton = styled.button`
-	border: none;
-	color: #ffb258;
-	cursor: pointer;
-	background-color: transparent;
-
-	&:hover {
-		color: #bd2130;
-	}
-`;
-
 const Header = () => {
+	const [logo, setLogo] = useState("");
+
 	const [loggedIn, setLoggedIn] = useRecoilState(loggedInState);
 	const navigate = useNavigate();
 
 	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 	const [notificationAnchorEl, setNotificationAnchorEl] =
 		useState<null | HTMLElement>(null);
+
+	useEffect(() => {
+		// 로고 이미지를 사전에 로딩.
+		const loadLogo = async () => {
+			const response = await fetch("public/logo/logo2.svg");
+			const imageBlob = await response.blob();
+			const imageObjectURL = URL.createObjectURL(imageBlob);
+			setLogo(imageObjectURL);
+		};
+
+		loadLogo();
+	}, []);
 
 	const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
 		setAnchorEl(event.currentTarget);
@@ -174,7 +91,7 @@ const Header = () => {
 			<Toolbar>
 				<Logo>
 					<Link to="/">
-						<img src="public/logo/logo2.svg" alt="모디 로고" />
+						{logo ? <img src={logo} alt="모디 로고" /> : "MODI"}
 					</Link>
 				</Logo>
 				<TextField
@@ -194,14 +111,16 @@ const Header = () => {
 				/>
 				{loggedIn && (
 					<>
-						<IconButton
+						<Button
+							startIcon={<FileUpload />}
+							variant="outlined"
 							color="inherit"
 							component={Link}
 							to="/productregistration"
+							sx={{ mr: 2 }}
 						>
-							<FileUpload />
 							업로드
-						</IconButton>
+						</Button>
 
 						<IconButton color="inherit" onClick={handleNotificationsMenuOpen}>
 							<Badge badgeContent={4} color="secondary">
