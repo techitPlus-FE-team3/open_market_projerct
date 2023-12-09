81cd152072d893fc90285c9d4f39a90a925c5500
an seungji <ansj8777@gmail.com>
2023-12-09 20:50:08 +0900

    ♻️[Refactor] 주석 제거

--- open-market/src/hooks/useRequireAuth.ts
+++ open-market/src/hooks/useRequireAuth.ts
@@ -9,7 +9,6 @@ export function useRequireAuth() {
 		const accessToken = localStorage.getItem("accessToken");
 		if (!accessToken) {
 			toast.error("로그인이 필요한 서비스입니다.");
-			// setTimeout(() => navigate("/signin"), 1500); // 토스트가 표시되는 동안 기다렸다가 리디렉트
 			navigate("/signin");
 		}
 	}, [navigate]);
