44ea3f790953bd84c4d72e731c52e66646cc6699
Pearl Kim <jinju000202@gmail.com>
2023-12-12 23:36:05 +0900

    :sparkles: [Feature] 장르 선택 컴포넌트 분리

---
+++ open-market/src/components/SelectGenre.tsx
@@ -0,0 +1,36 @@
+function SelectGenre({
+	id,
+	value,
+	handleFn,
+	category,
+}: {
+	id: string;
+	value?: string | undefined;
+	handleFn: React.ChangeEventHandler<HTMLSelectElement>;
+	category: CategoryCode[] | undefined;
+}) {
+	return (
+		<>
+			{category && value && (
+				<div>
+					<label htmlFor={id}>장르</label>
+					<select name={id} id={id} defaultValue={value} onChange={handleFn}>
+						<option value="none" disabled hidden>
+							장르를 선택해주세요
+						</option>
+						{category.length > 0 ? (
+							category.map((item) => (
+								<option key={item.code} value={item.code}>
+									{item.value}
+								</option>
+							))
+						) : (
+							<option disabled>아직 카테고리가 없습니다.</option>
+						)}
+					</select>
+				</div>
+			)}
+		</>
+	);
+}
+export default SelectGenre;
