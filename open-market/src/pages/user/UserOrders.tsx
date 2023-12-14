b571bfef065a7ae36ac63bf81e433327aa8fde7a
hungerbk <wjantlr@gmail.com>
2023-12-14 21:12:30 +0900

    :recycle:[Refactor] ProdcutListComponent 생성하여 분리 #232

--- open-market/src/pages/user/UserOrders.tsx
+++ open-market/src/pages/user/UserOrders.tsx
@@ -1,85 +1,24 @@
 import { FilterButton } from "@/components/FilterComponent";
+import {
+	FilterContainer,
+	Heading,
+	ProductContainer,
+	ProductList,
+	ProductSection,
+} from "@/components/ProductListComponent";
 import ProductListItem from "@/components/ProductListItem";
 import SearchBar from "@/components/SearchBar";
 import { useRequireAuth } from "@/hooks/useRequireAuth";
-import { Common } from "@/styles/common";
+
 import {
 	axiosInstance,
 	getItemWithExpireTime,
 	searchOrderList,
 	setItemWithExpireTime,
 } from "@/utils";
-import styled from "@emotion/styled";
+
 import { useEffect, useRef, useState } from "react";
 import { Helmet } from "react-helmet-async";
-
-const Heading = styled.h2`
-	display: ${Common.a11yHidden};
-`;
-
-const ProductSection = styled.section`
-	width: 1160px;
-	margin: 0 auto;
-	padding: ${Common.space.spacingLg};
-	display: flex;
-	flex-flow: column nowrap;
-	justify-content: center;
-	align-items: flex-end;
-	gap: ${Common.space.spacingMd};
-`;
-
-const ProductContainer = styled.div`
-	width: 1160px;
-	min-height: 633px;
-	padding: ${Common.space.spacingLg} 0 5px 0;
-	display: flex;
-	flex-flow: column nowrap;
-	align-items: center;
-	justify-content: space-between;
-	background-color: ${Common.colors.gray2};
-	border-radius: 10px;
-	box-shadow: 0px 5px 5px rgb(40, 40, 44, 0.3);
-
-	span.emptyList {
-		padding-top: 40px;
-		font-weight: ${Common.font.weight.regular};
-	}
-
-	.moreButton {
-		width: 100px;
-		height: 40px;
-		position: relative;
-		background-color: transparent;
-		border: none;
-		font-weight: ${Common.font.weight.regular};
-
-		&::after {
-			content: "";
-			position: absolute;
-			top: 50%;
-			transform: translateY(-30%);
-			right: 12px;
-			border-bottom: solid 8px transparent;
-			border-top: solid 8px black;
-			border-left: solid 8px transparent;
-			border-right: solid 8px transparent;
-		}
-	}
-`;
-
-const ProductList = styled.ul`
-	display: flex;
-	flex-flow: column nowrap;
-	align-items: center;
-	gap: ${Common.space.spacingLg};
-`;
-
-const FilterContainer = styled.div`
-	margin: ${Common.space.spacingLg} 0 5px ${Common.space.spacingMd};
-	display: flex;
-	flex-flow: row nowrap;
-	gap: ${Common.space.spacingMd};
-`;
 
 function UserOrders() {
 	const searchRef = useRef<HTMLInputElement>(null);
