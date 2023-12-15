af6160066fb9d726afc3a5a6b5f6d857d4ca017d
김진주 <83470333+pearlKinn@users.noreply.github.com>
2023-12-15 13:52:21 +0900

    💄 [Design] 상품 구매 페이지 레이아웃 구성 및 스타일링e #236 (#245)
    * :truck: [Rename] 변수명 수정
    
    * :label: [Type] 타입에 따른 스타일 변화
    
    * :lipstick: [Style] 상품 구매 페이지 레이아웃 구성 및 스타일링
    
    * :lipstick: [Design] content 박스 크기 조정

--- open-market/src/pages/product/ProductPurchase.tsx
+++ open-market/src/pages/product/ProductPurchase.tsx
@@ -1,14 +1,134 @@
+import FunctionalButton from "@/components/FunctionalButton";
+import Textarea from "@/components/Textarea";
 import { useRequireAuth } from "@/hooks/useRequireAuth";
-import { axiosInstance } from "@/utils";
+import { Common } from "@/styles/common";
+import { axiosInstance, numberWithComma } from "@/utils";
+import styled from "@emotion/styled";
 import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import toast from "react-hot-toast";
 import { useNavigate, useParams } from "react-router-dom";
 
+interface FlexLayoutProps {
+	right?: boolean;
+}
+
+interface StyleProps {
+	wide?: boolean;
+	large?: boolean;
+}
+
+interface LabelProps {
+	bar?: boolean;
+	large?: boolean;
+}
+
+const ProductPurchaseSection = styled.section`
+	background-color: ${Common.colors.white};
+	padding: 0 56px;
+
+	.a11yHidden {
+		display: ${Common.a11yHidden};
+	}
+
+	.ProductInfoWrapper {
+		background-color: ${Common.colors.gray2};
+		padding: 40px;
+		width: 1328px;
+		border-radius: 10px;
+		display: flex;
+		flex-direction: column;
+		gap: ${Common.space.spacingXl};
+	}
+	.ProductImage {
+		border-radius: 10px;
+		width: 300px;
+	}
+	.ProductInfo {
+		background-color: ${Common.colors.white};
+	}
+`;
+
+const FormTopLayout = styled.div`
+	width: 1248px;
+	display: flex;
+	gap: ${Common.space.spacingLg};
+`;
+
+const FormTopRightLayout = styled.div`
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+	gap: ${Common.space.spacingLg};
+	width: 918px;
+`;
+
+const FlexLayout = styled.div<FlexLayoutProps>`
+	display: flex;
+	gap: ${Common.space.spacingXl};
+	${(props) => props.right && "justify-content: flex-end;"}
+`;
+
+const ProductItemWrapper = styled.div<StyleProps>`
+	display: flex;
+	border-radius: 10px;
+	background-color: ${Common.colors.white};
+	padding: ${Common.space.spacingLg} ${Common.space.spacingMd};
+	${(props) => (props.wide ? "width: 677px;" : "flex-grow: 1;")}
+
+	${(props) =>
+		props.large
+			? `height: 290px; position: relative;`
+			: "height: 72px; 	align-items: center;"}
+`;
+
+const ProductLabel = styled.span<LabelProps>`
+	color: ${Common.colors.gray};
+	white-space: nowrap;
+
+	${(props) =>
+		props.bar &&
+		`
+    &:after {
+      content: " | ";
+    }
+  `}
+`;
+
+const ProductValue = styled.span<LabelProps>`
+	font-size: 16px;
+	color: ${Common.colors.black};
+	background-color: ${Common.colors.white};
+	padding: ${Common.space.spacingLg};
+	${(props) =>
+		props.large &&
+		`
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    font-size: ${Common.font.size.xl};
+    color:${Common.colors.gray}
+  `}
+`;
+
+const ContentWrapper = styled.div`
+	position: relative;
+
+	.ContentInHash {
+		color: ${Common.colors.gray};
+		padding: 5px ${Common.space.spacingMd} ${Common.space.spacingMd} 0px;
+		position: absolute;
+		right: 0;
+		bottom: 0;
+	}
+`;
+
 function ProductPurchase() {
 	const navigate = useNavigate();
 	const { productId } = useParams();
 	const [product, setProduct] = useState<Product>();
+	const [category, setCategory] = useState<CategoryCode[]>();
+	const [genre, setGenre] = useState<string>();
 
 	//비로그인 상태 체크
 	useRequireAuth();
@@ -61,45 +181,85 @@ function ProductPurchase() {
 		getProduct(productId!);
 	}, []);
 
+	useEffect(() => {
+		async function fetchCategory() {
+			try {
+				const response = await axiosInstance.get(`/codes/productCategory`);
+				const responseData = response.data.item;
+				const categoryCodeList = responseData.productCategory.codes;
+				setCategory(categoryCodeList);
+			} catch (error) {
+				console.error("상품 리스트 조회 실패:", error);
+			}
+		}
+
+		fetchCategory();
+	}, []);
+
+	useEffect(() => {
+		function translateCodeToValue(code: string) {
+			if (
+				code !== undefined &&
+				category !== undefined &&
+				product !== undefined
+			) {
+				return category.find((item) => item.code === code)?.value;
+			}
+		}
+		setGenre(translateCodeToValue(product?.extra?.category!));
+	}, [product, category]);
 	return (
-		<section>
+		<ProductPurchaseSection>
 			<Helmet>
 				<title>Order Product - 모두의 오디오 MODI</title>
 			</Helmet>
-			<h2>상품 구매</h2>
-			<div>
-				<div>
+			<h2 className="a11yHidden">상품 구매</h2>
+			<div className="ProductInfoWrapper">
+				<FormTopLayout>
 					<img
 						src={`${product?.mainImages[0].url}`}
 						alt={`${product?.name} 앨범 아트`}
+						className="ProductImage"
 					/>
-					<div>
-						<span>타이틀 {product?.name}</span>
-						<div>
-							<span>아티스트 {product?.seller_id}</span>
-							<span>장르 {product?.extra?.category}</span>
-						</div>
-						<div>
-							<span>설명</span>
-							<span>{product?.content}</span>
-							<span>{product?.extra?.tags?.map((tag) => `#${tag} `)}</span>
-						</div>
-					</div>
-				</div>
-				<div>
-					<span>결제 정보</span>
-					<span>{product?.price}₩</span>
-				</div>
-				<div>
-					<button type="button" onClick={() => navigate(-1)}>
-						취소
-					</button>
-					<button type="button" onClick={handleProductOrder}>
-						구매
-					</button>
-				</div>
+					<FormTopRightLayout>
+						<ProductItemWrapper>
+							<ProductLabel bar>제목</ProductLabel>
+							<ProductValue>{product?.name}</ProductValue>
+						</ProductItemWrapper>
+						<FlexLayout>
+							<ProductItemWrapper wide>
+								<ProductLabel bar>아티스트</ProductLabel>
+								<ProductValue> {product?.seller_id}</ProductValue>
+							</ProductItemWrapper>
+							<ProductItemWrapper>
+								<ProductLabel bar>장르</ProductLabel>
+								<ProductValue>{genre}</ProductValue>
+							</ProductItemWrapper>
+						</FlexLayout>
+						<ContentWrapper>
+							<Textarea readOnly={true} content={product?.content} small />
+							<span className="ContentInHash">
+								{product?.extra?.tags?.map((tag) => `#${tag} `)}
+							</span>
+						</ContentWrapper>
+					</FormTopRightLayout>
+				</FormTopLayout>
+				<ProductItemWrapper large>
+					<ProductLabel>결제 정보</ProductLabel>
+					<ProductValue large>
+						{product?.price !== undefined ? numberWithComma(product.price) : 0}₩
+					</ProductValue>
+				</ProductItemWrapper>
+				<FlexLayout right>
+					<FunctionalButton
+						secondary
+						handleFn={() => navigate(-1)}
+						text="취소"
+					/>
+					<FunctionalButton handleFn={handleProductOrder} text="구매" />
+				</FlexLayout>
 			</div>
-		</section>
+		</ProductPurchaseSection>
 	);
 }
 
