9b54f2a606ddd71bec6873623d35888489e660e6
an seungji <ansj8777@gmail.com>
2023-12-20 22:04:03 +0900

    💄[Design] 헤더 스타일 변경으로 인한 페이지 레이아웃 스타일링 변경(상품 등록 페이지) #276

--- open-market/src/pages/product/ProductRegistration.tsx
+++ open-market/src/pages/product/ProductRegistration.tsx
@@ -43,7 +43,8 @@ interface ProductRegistForm {
 }
 const ProductRegistSection = styled.section`
 	background-color: ${Common.colors.white};
-	padding: 0 56px;
+	padding-top: 100px;
+	padding-bottom: 20px;
 
 	.a11yHidden {
 		display: ${Common.a11yHidden};
@@ -53,6 +54,7 @@ const ProductRegistSection = styled.section`
 		background-color: ${Common.colors.gray2};
 		padding: 40px;
 		width: 1328px;
+		margin: 0 auto;
 		border-radius: 10px;
 		display: flex;
 		flex-direction: column;
