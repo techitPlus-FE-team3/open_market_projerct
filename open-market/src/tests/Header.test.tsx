4e5af70456a50f44abba67ca03f98700298f3073
an seungji <ansj8777@gmail.com>
2024-04-26 16:21:41 +0900

    ✅[test] 헤더 컴포넌트 로그인 상태에 따른 UI 변경 테스트 진행중 #371

--- open-market/src/tests/Header.test.tsx
+++ open-market/src/tests/Header.test.tsx
@@ -1,7 +1,7 @@
 import Header from "@/layout/Header";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { screen, render, waitFor } from "@testing-library/react";
-import { http } from "msw";
+import { HttpResponse, http } from "msw";
 import { setupServer } from "msw/node";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
@@ -10,15 +10,16 @@ import { afterAll, afterEach, beforeAll, describe, expect, it } from "vitest";
 const queryClient = new QueryClient();
 
 // API 모킹
-const server = setupServer(
-	http.get("/users/login", ({ req, res, ctx }) => {
-		return res(ctx.status(200), ctx.json([{ id: 1, name: "Product 1" }]));
-	}),
-);
+// const server = setupServer(
+// 	http.post("/users/login", async ({ request }) => {
+// 		const newPost = await request.json();
+// 		return HttpResponse.json(newPost, { status: 201 });
+// 	}),
+// );
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+// beforeAll(() => server.listen());
+// afterEach(() => server.resetHandlers());
+// afterAll(() => server.close());
 
 describe("Header 컴포넌트", () => {
 	it("로그인 상태에서 사용자 인터페이스를 테스트", async () => {
