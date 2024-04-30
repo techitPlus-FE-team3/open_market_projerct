506cbd60c891d8cb246f23d0149825436854bfd2
an seungji <ansj8777@gmail.com>
2024-04-30 11:13:26 +0900

    ✅[test] 헤더 컴포넌트 테스트 코드에 로그아웃 기능 테스트 코드 추가 #371

--- open-market/src/tests/Header.test.tsx
+++ open-market/src/tests/Header.test.tsx
@@ -1,13 +1,25 @@
 import Header from "@/layout/Header";
 import { currentUserState } from "@/states/authState";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { HttpResponse, http } from "msw";
 import { setupServer } from "msw/node";
 import { FunctionComponent, ReactNode, useEffect, useRef } from "react";
+import toast from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot, useSetRecoilState } from "recoil";
-import { afterAll, afterEach, beforeAll, describe, expect, it } from "vitest";
+import {
+	afterAll,
+	afterEach,
+	beforeAll,
+	describe,
+	expect,
+	it,
+	vi,
+} from "vitest";
+
+vi.mock("react-hot-toast");
 interface MockLoginStateProps {
 	children: ReactNode;
 	user: User | null;
@@ -157,5 +169,62 @@ describe("Header 컴포넌트", () => {
 		await waitFor(() =>
 			expect(screen.getByText("로그인 / 회원가입")).toBeInTheDocument(),
 		);
+	});
+
+	it("로그아웃 기능을 테스트", async () => {
+		// 사용자 정보를 초기화하고 로그인 상태로 설정
+		const user: User = {
+			_id: 1,
+			email: "test@example.com",
+			password: "password123",
+			name: "Test User",
+			phone: "123-456-7890",
+			type: "normal",
+			createdAt: "2021-01-01T00:00:00.000Z",
+			updatedAt: "2021-01-01T00:00:00.000Z",
+			extra: {
+				profileImage: "path/to/image.jpg",
+				terms: {
+					termsOfUse: true,
+					providingPersonalInformation: true,
+					recievingMarketingInformation: true,
+					confirmAge: true,
+				},
+			},
+			token: {
+				accessToken: "access-token",
+				refreshToken: "refresh-token",
+			},
+		};
+
+		render(
+			<QueryClientProvider client={queryClient}>
+				<RecoilRoot>
+					<BrowserRouter>
+						<MockLoginState user={user}>
+							<Header />
+						</MockLoginState>
+					</BrowserRouter>
+				</RecoilRoot>
+			</QueryClientProvider>,
+		);
+
+		// 로그아웃 버튼 클릭 시뮬레이션
+		const logoutButton = screen.getByTestId("logout-button");
+		await userEvent.click(logoutButton);
+
+		// 로컬 및 세션 스토리지에서 토큰이 제거되었는지 확인
+		expect(localStorage.getItem("accessToken")).toBeNull();
+		expect(localStorage.getItem("refreshToken")).toBeNull();
+		expect(sessionStorage.length).toBe(0);
+
+		// 리디렉션 및 알림 메시지가 표시되었는지 확인
+		await waitFor(() => {
+			expect(toast.success).toHaveBeenCalledWith(
+				"로그아웃 되었습니다.",
+				expect.anything(),
+			);
+			expect(window.location.pathname).toBe("/");
+		});
 	});
 });
