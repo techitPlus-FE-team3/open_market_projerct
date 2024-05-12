488404b224af6954855f6b080b11a786a010cee1
hungerbk <wjantlr@gmail.com>
2024-05-13 03:19:19 +0900

    :recycle:[Refactor] getUserProducts에 Promose 타입 추가 #381

--- open-market/src/apis/user/products.ts
+++ open-market/src/apis/user/products.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from "@/utils";
 
-export async function getUserProducts() {
+export async function getUserProducts(): Promise<Product[] | undefined> {
 	const response = await axiosInstance.get(`/seller/products/`);
 	return response.data.item;
 }
