72da7443c60faf91df2367938c9e7e4f4d3fbc9c
hungerbk <wjantlr@gmail.com>
2024-05-14 11:56:47 +0900

    :sparkles:[Feat] delete bookmark api 생성 #381

--- open-market/src/apis/user/bookmarks.ts
+++ open-market/src/apis/user/bookmarks.ts
@@ -4,3 +4,8 @@ export async function getUserBookmarks() {
 	const response = await axiosInstance.get(`/bookmarks`);
 	return response.data.item;
 }
+
+export async function deleteUserBookmark(bookmarkId: string) {
+	const response = await axiosInstance.delete(`/bookmarks/${bookmarkId}`);
+	return response.data;
+}
