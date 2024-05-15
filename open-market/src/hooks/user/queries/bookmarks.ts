aee5584da6910971e2c7d5f72f00182392db5e12
hungerbk <wjantlr@gmail.com>
2024-05-16 01:55:22 +0900

    :recycle:[Refactor] useUserBookmarksQuery return 객체 수정 #381

--- open-market/src/hooks/user/queries/bookmarks.ts
+++ open-market/src/hooks/user/queries/bookmarks.ts
@@ -2,11 +2,11 @@ import { getUserBookmarks } from "@/apis/user/bookmarks";
 import { useQuery } from "@tanstack/react-query";
 
 export function useUserBookmarksQuery() {
-	const { data, ...rest } = useQuery({
+	const { data, isLoading } = useQuery({
 		queryKey: ["userBookmarks"],
 		queryFn: () => getUserBookmarks(),
 		select: (data) => [...data].reverse(),
 	});
 
-	return { data, ...rest };
+	return { data, isLoading };
 }
