7dcc42a3625eb5d487171c6820a06c37539c450e
hungerbk <wjantlr@gmail.com>
2024-05-16 02:08:00 +0900

    :sparkles:[Feat] useUserOrdersInfiniteQuery 생성 #381

--- open-market/src/hooks/user/queries/orders.ts
+++ open-market/src/hooks/user/queries/orders.ts
@@ -1,9 +1,21 @@
-import { getUserOrders } from "@/apis/user/orders";
-import { useQuery } from "@tanstack/react-query";
+import { getUserOrders, getUserOrdersWithPageParam } from "@/apis/user/orders";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 export function useUserOrdersQuery() {
 	return useQuery({
 		queryKey: ["userOrders"],
 		queryFn: () => getUserOrders(),
+	});
+}
+
+export function useUserOrdersInfiniteQuery() {
+	return useInfiniteQuery({
+		queryKey: ["userOrdersInfinite"],
+		queryFn: getUserOrdersWithPageParam,
+		initialPageParam: 1,
+		getNextPageParam: (lastPage) =>
+			lastPage.pagination.page < lastPage.pagination.totalPages
+				? lastPage.pagination.page + 1
+				: null,
 	});
 }
