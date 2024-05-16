0cb33c19fc4881d386aee1953ad3c5ec801b1078
an seungji <ansj8777@gmail.com>
2024-05-16 11:23:35 +0900

    ♻️[Refactor] 회원정보 수정 페이지 api 호출부 분리를 위한 코드 구축 #379

--- open-market/src/apis/user/auth.ts
+++ open-market/src/apis/user/auth.ts
@@ -15,3 +15,8 @@ export async function signUp(userData: SignUpRequest) {
 	const response = await axiosInstance.post("/users/", userData);
 	return response.data;
 }
+
+export async function updateUser(userId: string, userData: UpdateUserRequest) {
+	const response = await axiosInstance.patch(`/users/${userId}`, userData);
+	return response.data;
+}
