c99c6f08a2f6329056bd0815237abb15905a6136
Pearl Kim <jinju000202@gmail.com>
2024-05-17 15:17:02 +0900

    [Bug] 깃 버그로 인한 코드 수정

--- open-market/src/apis/user/bookmarks.ts
+++ open-market/src/apis/user/bookmarks.ts
@@ -1,15 +1,23 @@
 import { axiosInstance } from "@/utils";
 
-export async function getUserBookmarks(
-	productId?: string,
-): Promise<Bookmark | undefined> {
-	try {
-		const response = await axiosInstance.get(
-			`/bookmarks/products/${productId}`,
-		);
+export async function getUserBookmarks() {
+	const response = await axiosInstance.get(`/bookmarks`);
+	return response.data.item;
+}
 
-		return response.data.item;
-	} catch (error) {
-		console.error(error);
-	}
+export async function deleteUserBookmark(bookmarkId: string | number) {
+	const response = await axiosInstance.delete(`/bookmarks/${bookmarkId}`);
+	return response.data;
+}
+
+export async function postUserBookmark(
+	currentUserId: string | number,
+	productId: string | number,
+) {
+	const response = await axiosInstance.post(`/bookmarks/`, {
+		user_id: currentUserId,
+		product_id: productId,
+		memo: "",
+	});
+	return response.data;
 }
