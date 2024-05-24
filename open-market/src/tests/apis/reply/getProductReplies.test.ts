e779782ae1692e46a8b1619ca1a99385b8f4dac7
hungerbk <wjantlr@gmail.com>
2024-05-25 06:11:35 +0900

    :white_check_mark:[Test] getProductReplies api 테스트 코드 작성 #373

---
+++ open-market/src/tests/apis/reply/getProductReplies.test.ts
@@ -0,0 +1,63 @@
+import { getProductReplies } from "@/apis/product/replies";
+import { axiosInstance } from "@/utils";
+import { afterEach, describe, expect, it, vi } from "vitest";
+
+vi.mock("@/utils", () => {
+	return {
+		axiosInstance: {
+			get: vi.fn(),
+		},
+	};
+});
+
+describe("getProductReplies", () => {
+	const productId = "45";
+
+	const repliesDataMock = {
+		_id: 0,
+		order_id: 0,
+		product_id: Number(productId),
+		rating: 5,
+		content: "",
+		createdAt: "",
+		user: {
+			name: "",
+			_id: 0,
+		},
+		extra: {
+			profileImage: "",
+		},
+	};
+
+	afterEach(() => {
+		vi.resetAllMocks();
+	});
+
+	it("상품 댓글 정보를 불러온다.", async () => {
+		const mockResponse = {
+			data: {
+				item: {
+					repliesDataMock,
+				},
+			},
+		};
+		axiosInstance.get = vi.fn().mockResolvedValue(mockResponse);
+
+		const result = await getProductReplies(productId);
+
+		expect(result).toEqual(mockResponse.data.item);
+		expect(axiosInstance.get).toHaveBeenCalledWith(
+			`/replies/products/${productId}`,
+		);
+	});
+
+	it("상품 댓글 정보를 불러오지 못했을 때 에러가 발생한다.", async () => {
+		const mockError = new Error("Network Error");
+		axiosInstance.get = vi.fn().mockRejectedValue(mockError);
+
+		await expect(getProductReplies(productId)).rejects.toThrow("Network Error");
+		expect(axiosInstance.get).toHaveBeenCalledWith(
+			`/replies/products/${productId}`,
+		);
+	});
+});
