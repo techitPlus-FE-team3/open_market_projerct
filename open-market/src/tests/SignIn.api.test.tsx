6f19cf25d5e3c04d1322a69f54f8c0a86aba94f6
an seungji <ansj8777@gmail.com>
2024-05-25 00:31:38 +0900

    ♻️ [refactor] 리팩토링 이후 회원가입 페이지 기능 테스트 코드 원복 (테스트 코드 구성 방향성 재고중...) #392

--- open-market/src/tests/SignIn.api.test.tsx
+++ open-market/src/tests/SignIn.api.test.tsx
@@ -1,73 +1,61 @@
 import SignIn from "@/pages/user/SignIn";
-import { signIn } from "@/apis/user/auth";
+import { axiosInstance } from "@/utils";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { HelmetProvider } from "react-helmet-async";
 import toast from "react-hot-toast";
 import { MemoryRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { beforeEach, describe, expect, it, vi, Mock } from "vitest";
-
-vi.mock("@/apis/user/auth", () => ({
-	signIn: vi.fn(),
-}));
+import { beforeEach, describe, expect, it, vi } from "vitest";
 
 vi.mock("@/utils/debounce", () => ({
 	debounce: (fn: Function) => fn, // 디바운스를 무시하고 즉시 실행되도록 변경
 }));
 
-// vi.mock("react-hot-toast");
-
-vi.mock("react-hot-toast", () => ({
-	success: vi.fn(),
-	error: vi.fn(),
-}));
+vi.mock("react-hot-toast");
 
 describe("로그인 페이지 기능 테스트", () => {
-	let queryClient: QueryClient;
-
 	beforeEach(() => {
 		vi.resetAllMocks();
-		queryClient = new QueryClient();
+		axiosInstance.post = vi
+			.fn()
+			.mockImplementation(
+				(_url: string, data: { email: string; password: string }) => {
+					if (data.email === "u1@market.com" && data.password === "11111111") {
+						return Promise.resolve({
+							data: {
+								ok: 1,
+								item: {
+									token: {
+										accessToken: "access-token",
+										refreshToken: "refresh-token",
+									},
+									_id: "user-id",
+									name: "user-name",
+								},
+							},
+						});
+					} else {
+						return Promise.reject({
+							response: {
+								data: {
+									message: "로그인 실패",
+									errors: ["이메일 혹은 비밀번호가 정확하지 않습니다."],
+								},
+								status: 401,
+							},
+						});
+					}
+				},
+			);
 	});
 
 	it("로그인 시 API 호출, 리디렉션 및 토스트 알림 출력 테스트", async () => {
-		(signIn as Mock).mockImplementation((email: string, password: string) => {
-			if (email === "u1@market.com" && password === "11111111") {
-				return Promise.resolve({
-					data: {
-						ok: 1,
-						item: {
-							token: {
-								accessToken: "access-token",
-								refreshToken: "refresh-token",
-							},
-							_id: "user-id",
-							name: "user-name",
-						},
-					},
-				});
-			} else {
-				return Promise.reject({
-					response: {
-						data: {
-							message: "로그인 실패",
-							errors: ["이메일 혹은 비밀번호가 정확하지 않습니다."],
-						},
-						status: 401,
-					},
-				});
-			}
-		});
-
 		render(
 			<RecoilRoot>
 				<MemoryRouter>
 					<HelmetProvider>
-						<QueryClientProvider client={queryClient}>
-							<SignIn />
-						</QueryClientProvider>
+						<SignIn />
 					</HelmetProvider>
 				</MemoryRouter>
 			</RecoilRoot>,
@@ -83,9 +71,12 @@ describe("로그인 페이지 기능 테스트", () => {
 		await userEvent.click(loginButton);
 
 		await waitFor(() => {
-			expect(signIn).toHaveBeenCalledWith("u1@market.com", "11111111");
+			expect(axiosInstance.post).toHaveBeenCalledWith("/users/login", {
+				email: "u1@market.com",
+				password: "11111111",
+			});
+			expect(window.location.pathname).toBe("/");
 		});
-
 		// 로그인 성공 메시지 확인
 		await waitFor(() => {
 			expect(toast.success).toHaveBeenCalledWith(
@@ -93,32 +84,14 @@ describe("로그인 페이지 기능 테스트", () => {
 				expect.anything(),
 			);
 		});
-
-		await waitFor(() => {
-			expect(window.location.pathname).toBe("/");
-		});
 	});
 
 	it("로그인 실패 시 토스트 알림 출력 테스트", async () => {
-		(signIn as Mock).mockImplementation(() => {
-			return Promise.reject({
-				response: {
-					data: {
-						message: "로그인 실패",
-						errors: ["이메일 혹은 비밀번호가 정확하지 않습니다."],
-					},
-					status: 401,
-				},
-			});
-		});
-
 		render(
 			<RecoilRoot>
 				<MemoryRouter>
 					<HelmetProvider>
-						<QueryClientProvider client={queryClient}>
-							<SignIn />
-						</QueryClientProvider>
+						<SignIn />
 					</HelmetProvider>
 				</MemoryRouter>
 			</RecoilRoot>,
