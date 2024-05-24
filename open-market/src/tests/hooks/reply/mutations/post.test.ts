99310934bfe8d88f15c880eca0aa1e46f44f617b
hungerbk <wjantlr@gmail.com>
2024-05-24 16:49:14 +0900

    :white_check_mark:[Test] 댓글 작성에 성공했을 때 쿼리 데이터 업데이트 확인 테스트 코드 추가 #373

--- open-market/src/tests/hooks/reply/mutations/post.test.ts
+++ open-market/src/tests/hooks/reply/mutations/post.test.ts
@@ -1,4 +1,5 @@
 import { postProductReply } from "@/apis/product/replies";
+import { useProductRepliesQuery } from "@/hooks/product/queries/reply";
 import { usePostReplyMutation } from "@/hooks/reply/mutations/usePostReplyMutation";
 import TestWrapper from "@/tests/Wrapper";
 import { renderHook, waitFor } from "@testing-library/react";
@@ -13,12 +14,12 @@ vi.mock("@/apis/product/replies", () => ({
 const mockPostProductReply = postProductReply as unknown as MockInstance;
 
 describe("usePostReplyMutation", () => {
-	const mockReplyContent = "정말 좋네요!";
+	const mockProductId = "45";
 	const mockReply = {
 		order_id: 0,
-		product_id: 45,
+		product_id: Number(mockProductId),
 		rating: 5,
-		content: mockReplyContent,
+		content: "정말 좋네요!",
 		extra: {
 			profileImage: "",
 		},
@@ -32,34 +33,51 @@ describe("usePostReplyMutation", () => {
 		vi.clearAllMocks();
 	});
 
-	it("댓글 작성이 성공하면, 성공했다는 toast가 보여진다.", async () => {
+	it("댓글 작성이 성공하면, 성공했다는 toast가 보여지고, 쿼리 데이터가 업데이트된다.", async () => {
 		mockPostProductReply.mockResolvedValueOnce(mockReply);
+
 		const toastSuccessSpy = vi.spyOn(toast, "success");
 
-		const { result } = renderHook(() => usePostReplyMutation(), {
-			wrapper: TestWrapper,
-		});
+		const { result: mutationResult } = renderHook(
+			() => usePostReplyMutation({ productId: mockProductId }),
+			{
+				wrapper: TestWrapper,
+			},
+		);
+
+		const { result: queryResult } = renderHook(
+			() => useProductRepliesQuery({ productId: mockProductId }),
+			{ wrapper: TestWrapper },
+		);
 
 		await waitFor(async () => {
-			result.current.mutate(mockReply);
+			mutationResult.current.mutate(mockReply);
 		});
 
 		expect(toastSuccessSpy).toHaveBeenCalledWith(
 			"댓글을 작성했습니다.",
 			expect.anything(),
 		);
+
+		await waitFor(() => {
+			expect(queryResult.current.data).toContainEqual(mockReply);
+		});
 	});
 
 	it("댓글 작성이 실패하면, 에러를 console.error()로 확인할 수 있다", async () => {
 		mockPostProductReply.mockRejectedValueOnce(new Error("Network Error"));
 
-		const { result } = renderHook(() => usePostReplyMutation(), {
-			wrapper: TestWrapper,
-		});
 		const consoleErrorSpyOn = vi.spyOn(console, "error");
 
+		const { result: mutationResult } = renderHook(
+			() => usePostReplyMutation({ productId: mockProductId }),
+			{
+				wrapper: TestWrapper,
+			},
+		);
+
 		await waitFor(async () => {
-			result.current.mutate(mockReply);
+			mutationResult.current.mutate(mockReply);
 		});
 
 		expect(consoleErrorSpyOn).toHaveBeenCalled();
