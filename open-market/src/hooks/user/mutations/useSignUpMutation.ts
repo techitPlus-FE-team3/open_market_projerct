23f8d99500ed4b4a11dee126eaa3bc3e5041dfe9
hungerbk <wjantlr@gmail.com>
2024-05-25 16:28:02 +0900

    :art:[Style] import 순서 수정 #396

--- open-market/src/hooks/user/mutations/useSignUpMutation.ts
+++ open-market/src/hooks/user/mutations/useSignUpMutation.ts
@@ -1,7 +1,7 @@
-import { useMutation } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
 import { signUp } from "@/apis/user/auth";
+import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export function useSignUpMutation() {
 	const navigate = useNavigate();
